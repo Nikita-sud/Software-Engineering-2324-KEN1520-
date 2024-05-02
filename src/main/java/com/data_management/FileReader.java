8b70690b3a39ec2bde749df6387306a1e545882a
Mona Lisa <MonaLisa@email>
2024-05-02 12:39:20 +0200

    Updated FileReader.java

--- src/main/java/com/data_management/FileReader.java
+++ src/main/java/com/data_management/FileReader.java
@@ -3,16 +3,12 @@ package com.data_management;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.stream.Stream;
 
 public class FileReader implements DataReader{
-    // variable stores path to the file
-    private String filePath;
-
-    public FileReader(String filepath){
-        this.filePath = filePath;
-    }
     
     /**
      * reads data from a specified source and stores it in the data storage.
@@ -20,24 +16,29 @@ public class FileReader implements DataReader{
      * @param dataStorage the storage where data will be stored
      * @throws IOException if there is an error reading the data
      */
-    public void readData(DataStorage dataStorage) throws IOException{
-        try(BufferedReader reader = Files.newBufferedReader(Paths.get(filePath))){
-            String line;
-            while ((line =reader.readLine())!=null) {
-                if(filePath.endsWith(".csv")){
-                    // Split the line on the words and assign each word to the specific variable
-                    String[] arrLineWords= line.split(",");
-                    int patientId = Integer.parseInt(arrLineWords[0]);
-                    double measurementValue = Double.parseDouble(arrLineWords[1]);
-                    String recordType = arrLineWords[2];
-                    long timestamp = Long.parseLong(arrLineWords[3]);
-                    // Add new data to the dataStorage
-                    dataStorage.addPatientData(patientId,measurementValue,recordType, timestamp);
-                }
-            }
+    public void readData(DataStorage dataStorage) throws IOException {
+        String directoryPath = "src\\main\\java\\com\\cardio_generator\\outputs";
 
-        } catch (Exception IOException){
-            throw new IOException("Error in reading data");
+        try (Stream<Path> paths = Files.walk(Paths.get(directoryPath))) {
+            paths.filter(Files::isRegularFile)
+                .filter(path -> path.toString().endsWith(".txt"))
+                .forEach(path -> {
+                    try (BufferedReader reader = Files.newBufferedReader(path)) {
+                        String line;
+                        while ((line = reader.readLine()) != null) {
+                            String[] arrLineWords = line.split(",");
+                            int patientId = Integer.parseInt(arrLineWords[0]);
+                            double measurementValue = Double.parseDouble(arrLineWords[1]);
+                            String recordType = arrLineWords[2];
+                            long timestamp = Long.parseLong(arrLineWords[3]);
+                            dataStorage.addPatientData(patientId, measurementValue, recordType, timestamp);
+                        }
+                    } catch (IOException e) {
+                        System.err.println("Error reading file: " + path);
+                    }
+                });
+        } catch (IOException e) {
+            throw new IOException("Error walking through directory: " + directoryPath, e);
         }
     }
 }
