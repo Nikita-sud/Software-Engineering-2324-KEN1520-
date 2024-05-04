32ba73c9f154adfa28b996b4d7e8d85b04f93cd1
Mona Lisa <MonaLisa@email>
2024-05-04 14:00:35 +0200

    Made a  few more tests for the data managment

--- src/test/java/data_management/DataStorageANDFileReaderTest.java
+++ src/test/java/data_management/DataStorageANDFileReaderTest.java
@@ -6,11 +6,13 @@ import org.junit.jupiter.api.Test;
 import com.data_management.DataReader;
 import com.data_management.DataStorage;
 import com.data_management.FileReader;
+import com.data_management.Patient;
 import com.data_management.PatientRecord;
 
+import java.util.ArrayList;
 import java.util.List;
 
-class DataStorageTest {
+class DataStorageANDFileReaderTest {
 
     @Test
     void testAddAndGetRecords() {
@@ -28,5 +30,16 @@ class DataStorageTest {
         List<PatientRecord> records = storage.getRecords(10, 1714748468033L, 1714748468034L);
         assertEquals(2, records.size()); // Check if two records are retrieved
         assertEquals(-0.34656395320945643, records.get(0).getMeasurementValue()); // Validate first record
+        
+        List<Patient> allRecords = storage.getAllPatients();
+        assertEquals(1, allRecords.get(1).getPatientId()); // Check the first patient id 
+        assertEquals(31, allRecords.get(7).getPatientId()); // Check the last patient id 
+    }
+
+    @Test
+    void testGetRecordsNullBehaviour() {
+        DataStorage storage = new DataStorage();
+        List<PatientRecord> records = storage.getRecords(10, 1714748468033L, 1714748468034L);
+        assertEquals(0, records.size()); // Check if the new empty Array was created
     }
 }
