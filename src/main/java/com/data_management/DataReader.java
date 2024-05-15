6ea35efeb28f18778324ffea200b85205d0f190d
Mona Lisa <MonaLisa@email>
2024-05-15 16:57:07 +0200

    test

--- src/main/java/com/data_management/DataReader.java
+++ src/main/java/com/data_management/DataReader.java
@@ -4,13 +4,7 @@ import java.io.IOException;
 import java.net.URI;
 
 public interface DataReader {
-    /**
-     * Reads data from a specified source and stores it in the data storage.
-     * 
-     * @param dataStorage the storage where data will be stored
-     * @throws IOException if there is an error reading the data
-     */
-    void readDataFromFile(DataStorage dataStorage) throws IOException;
+
 
     /**
      * Connects to a WebSocket server and stores received data in the data storage.
